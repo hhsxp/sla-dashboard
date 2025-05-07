1ed2046f5613eebf150c0851e9664672fa37cf26
hhsxp <beto_hhs@hotmail.com>
2025-05-07 12:48:47 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // opcionalmente, se você quiser que o HTML seja gerado
-  // com trailing slash: '/about/' em vez de '/about'
-  // trailingSlash: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
