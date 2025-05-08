942b7cd2cb5c1a19ac3d562d970347c9b383c3d2
hhsxp <beto_hhs@hotmail.com>
2025-05-08 11:30:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // retire qualquer swcMinify ou output: 'export'
-};
-
-module.exports = nextConfig;
+  output: 'standalone'  // sem `export`
+}
+module.exports = nextConfig
