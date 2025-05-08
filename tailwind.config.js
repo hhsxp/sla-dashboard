31420956b0cf3c372b5139a27ce87517379e13f8
hhsxp <beto_hhs@hotmail.com>
2025-05-08 09:48:13 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins','sans-serif'],
-        lora: ['Lora','serif'],
-      },
-      colors: {
-        slaOnTime: '#28a745',
-        slaBreached: '#dc3545',
-        atRisk: '#ffc107',
-      }
-    }
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
