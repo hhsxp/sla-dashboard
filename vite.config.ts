0a79a5c98d2ae8dd24c2d22ddc299d6579724c61
hhsxp <beto_hhs@hotmail.com>
2025-05-21 13:44:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',      // <— caminha para assets relativos
   plugins: [react()]
 });
